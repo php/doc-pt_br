a765b610d2e70eb097309cbef32e954e6b2c717e
André L F S Bacci <alfsb@example.com>
2023-06-19 12:59:20 -0300

    Incrementa ferramentas de sincronia de ws.

--- .tools/test/test.sh
+++ .tools/test/test.sh
@@ -18,11 +18,12 @@ if ! $(cd pt_BR; git apply --check ../patch.txt); then
 else
 
     (cd pt_BR; git apply ../patch.txt 2>&1 | tee ../merge.txt;)
-    (cd pt_BR; git diff --name-only > ../list.txt )
+    (cd pt_BR; git diff --name-only > ../list.txt)
 
     php doc-base/configure.php --with-lang=pt_BR --enable-xml-details    
     (cd pt_BR; git status)
     (cd pt_BR; git diff)
+    (cd pt_BR; git diff --name-only)
 
     echo
     echo "If OK, accept the pull on GitHub and then "
