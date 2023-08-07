d426a9c3f8ce76e79c8f27c21b2c2d41b367404e
André L F S Bacci <alfsb@example.com>
2023-08-07 14:02:35 -0300

    Rejeita push em repositórios/forks atrasados.

--- .tools/test/push.sh
+++ .tools/test/push.sh
@@ -7,10 +7,17 @@ fi
 set +e
 cd pt_BR
 
-git pull --rebase --autostash
-git commit -a -m "$msg"
-git push
-git status
+behind=0
+LC_ALL=C git status -sb | grep -q 'behind' && behind=1
+if [ $behind = 1 ]; then
+    echo "Algum repositório desatualizado."
+    echo "Utilize 'git status' para verificar e 'git pull --rebase --autostash' para sincronizar."
+    exit
+else
+    git commit -a -m "$msg"
+    git push
+    git status
+fi
 
 set -e
 cd ..
