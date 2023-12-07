2d584edda08b9498194c191d25e065f471f09b83
André L F S Bacci <ae@php.net>
2023-12-07 15:12:17 -0300

    Copyright e licença do código

--- .tools/test/mess.php
+++ .tools/test/mess.php
@@ -1,11 +1,22 @@
 <?php
-
 /**
- * mess.php -- Gera um arquivo "conflitado", contendo as remoções
- * e inclusões de linhas, respectivamente da tradução e do original,
- * a partir de um diff do hash informado.
- *
- * EXPERIMENTAL. Modifica o arquivo destino sem outras verificações.
+ *  +----------------------------------------------------------------------+
+ *  | Copyright (c) 1997-2023 The PHP Group                                |
+ *  +----------------------------------------------------------------------+
+ *  | This source file is subject to version 3.01 of the PHP license,      |
+ *  | that is bundled with this package in the file LICENSE, and is        |
+ *  | available through the world-wide-web at the following url:           |
+ *  | https://www.php.net/license/3_01.txt.                                |
+ *  | If you did not receive a copy of the PHP license and are unable to   |
+ *  | obtain it through the world-wide-web, please send a note to          |
+ *  | license@php.net, so we can mail you a copy immediately.              |
+ *  +----------------------------------------------------------------------+
+ *  | Authors:     Andŕe L F S Bacci <ae php.net>                          |
+ *  +----------------------------------------------------------------------+
+ *  | Description: Gera um arquivo anotado, indicando as inclusões e       |
+ *  |              remoções de linhas conforme diff do hash informado.     |
+ *  |              EXPERIMENTAL.                                           |
+ *  +----------------------------------------------------------------------+
  */
 
 if ( $argc == 2)
--- .tools/test/spce.php
+++ .tools/test/spce.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ *  +----------------------------------------------------------------------+
+ *  | Copyright (c) 1997-2023 The PHP Group                                |
+ *  +----------------------------------------------------------------------+
+ *  | This source file is subject to version 3.01 of the PHP license,      |
+ *  | that is bundled with this package in the file LICENSE, and is        |
+ *  | available through the world-wide-web at the following url:           |
+ *  | https://www.php.net/license/3_01.txt.                                |
+ *  | If you did not receive a copy of the PHP license and are unable to   |
+ *  | obtain it through the world-wide-web, please send a note to          |
+ *  | license@php.net, so we can mail you a copy immediately.              |
+ *  +----------------------------------------------------------------------+
+ *  | Authors:     Andŕe L F S Bacci <ae php.net>                          |
+ *  +----------------------------------------------------------------------+
+ *  | Description: This file trim whitespace at end, compares and          |
+ *  |              syncronize starting whitespace between two files.       |
+ *  +----------------------------------------------------------------------+
+ */
 
 wsfix ( $argv[1] );
 
